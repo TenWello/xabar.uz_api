edeb70d9e101d05d26394b56fad2d117ae45d7bb
Fayziyev Jahongir <jahongirfayziyev94@gmail.com>
2025-07-08 15:12:38 +0500

    xabar.uz latest news api

--- news/management/commands/fetch_xabaruz.py
+++ news/management/commands/fetch_xabaruz.py
@@ -23,36 +23,28 @@ class Command(BaseCommand):
         resp.raise_for_status()
         soup = BeautifulSoup(resp.text, "html.parser")
 
-        # "So‘nggi yangiliklar" konteyneri
         main_news = soup.find("div", class_="latest__news-text")
         print("main_news_container:", bool(main_news))
         if not main_news:
             self.stdout.write(self.style.ERROR("Yangiliklar konteyneri topilmadi!"))
             return
 
-        # Barcha yangilik elementlari (rasmli va rasmisiz)
         news_boxes = main_news.select("div.news__item.clickable-block")
         print("Topildi:", len(news_boxes), "ta yangilik")
 
         for box in news_boxes:
-            # Sarlavha va link
             title_tag = box.select_one("p.news__item-title a")
             title = title_tag.text.strip() if title_tag else ""
             link  = urljoin(base_url, title_tag["href"]) if title_tag else ""
-
-            # Vaqt ("Ҳозиргина", "12 дақиқа олдин", "1 соат олдин" yoki "бугун, 11:29")
             meta_tag = box.select_one("p.news__item-meta")
             time_ago = meta_tag.text.strip() if meta_tag else ""
 
-            # Qisqacha matn (tagida class bo‘lmagan birinchi <p>)
             desc_tag = box.select_one("p:not([class])")
             description = desc_tag.text.strip() if desc_tag else ""
 
-            # Rasm (agar bo‘lsa)
             img_tag = box.select_one("img")
             image = urljoin(base_url, img_tag["src"]) if img_tag and img_tag.has_attr("src") else ""
 
-            # Saqlash
             News.objects.update_or_create(
                 link=link,
                 defaults={
@@ -61,7 +53,6 @@ class Command(BaseCommand):
                     "image": image,
                     "category": "",
                     'time_ago': time_ago,
-                    # Agar hozircha narsa bo‘lmasa:
                     "published_at": None,
                 }
             )
